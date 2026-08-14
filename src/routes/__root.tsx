bcc720ee06359d61fe3fdfaa2f376bdb4b4dd669
Alvin11David <alvin69david@gmail.com>
2026-08-14 10:45:08 +0300

    auto-commit: M src/routes/__root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -214,6 +214,7 @@ function RootComponent() {
         <ServiceWorkerRegister />
         <PwaInstallPrompt />
         <AndroidApkPrompt />
+        <AndroidDownloadButton />
         <Outlet />
       </AuthProvider>
     </QueryClientProvider>
